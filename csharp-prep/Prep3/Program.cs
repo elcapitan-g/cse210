01cf6059655c556f79f8e73231c2ce8240e08952
Your Nameelcapitan-g <gnewell89@gmail.com>
2024-10-31 13:14:58 -1000

    Finsihed the first 3 excercises
    	finished:   csharp-prep/Prep1/Program.cs
    	finsihed:   csharp-prep/Prep2/Program.cs
    	finished:   csharp-prep/Prep3/Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
 
 class Program
-{
-    static void Main(string[] args)
+{static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your number guess? ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out guess))
+            {
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! You have guessed the number!");
+                }
+            }
+
+        }
     }
 }
